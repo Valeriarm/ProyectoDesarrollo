2ca7f293acfa85a3353e8f3727b9223919df0e90
Valeria <valeria.rivera@correounivalle.edu.co>
2018-10-11 16:36:27 -0500

    Se creo la fachada que hace la conexión con la base de datos

---
+++ ProyectoDesarrollo/src/Access/Fachada.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Access;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Asus
+ */
+public class Fachada {
+        String url, usuario, password;
+        Connection conexion =null;
+        Statement instruccion;
+        ResultSet tabla;
+        
+        Fachada(){
+            url="jdbc:postgresql://localhost:5433/muebles_XYZ";
+            usuario="desarrollo";
+            password="desarrollo";
+        }
+        
+        
+
+       public Connection conectar(){
+            try {
+            // Se carga el driver
+            Class.forName("org.postgresql.Driver");
+            //System.out.println( "Driver Cargado" );
+            } catch( Exception e ) {
+                System.out.println( "No se pudo cargar el driver." );
+            }
+
+            try{
+                     //Crear el objeto de conexion a la base de datos
+                     conexion = DriverManager.getConnection(url, usuario, password);
+                     System.out.println( "Conexion Abierta" );
+                     return conexion;
+                  //Crear objeto Statement para realizar queries a la base de datos
+             } catch( Exception e ) {
+                System.out.println( "No se pudo abrir la bd." );
+                return null;
+             }
+
+        }//end connectar
+
+        public Connection getConnetion(){
+            if (conexion == null) {
+                return this.conectar();
+            }
+            else{
+                  return conexion;      
+            }
+            
+        }
+        
+        public void closeConection(Connection c){
+            try{
+                if (conexion != null){
+                    c.close();
+                }
+                 
+            } catch( Exception e ) {
+                System.out.println( "No se pudo cerrar." );
+            }
+        }
+        
+        
+        public static void main(String args[]){
+            Fachada f = new Fachada();
+            f.getConnetion();
+        }
+}
